4b9312c3ca1006faec2b8852fbdb7ee02718dcfb
RavneetK <rkaur@eclsys.com>
2014-09-05 16:19:04 +0530

    hi

---
+++ Receiving/Models/CodeDescription.cs
@@ -0,0 +1,14 @@
+﻿
+namespace DcmsMobile.Receiving.Models
+{
+    public class CodeDescription
+    {
+        public string Code { get; set; }
+
+        public string Description { get; set; }
+    }
+}
+
+
+
+//$Id: CodeDescription.cs 8483 2011-12-15 11:31:00Z rkandari $
